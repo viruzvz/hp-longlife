058d2e51079b4933074d6816eae66587c2178149
@cegn <cegn@senado.leg.br>
2024-01-04 10:14:20 -0300

    script count when it get view

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,20 @@
 // Import all of Bootstrap's JS
 import * as bootstrap from 'bootstrap'
 
+// Remove the transition class
+const square = document.querySelector('.status-fire-js');
+square.classList.remove('here');
+
+// Create the observer, same as before:
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      square.classList.add('here');
+      return;
+    }
+
+    square.classList.remove('here');
+  });
+});
+
+observer.observe(document.querySelector('.square-wrapper'));
