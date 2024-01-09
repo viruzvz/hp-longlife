3c506a55f15c1d3f2df058a0450827f3d59a3437
@cegn <cegn@senado.leg.br>
2024-01-08 22:14:59 -0300

    projeto finalizado

--- src/js/main.js
+++ src/js/main.js
@@ -2,23 +2,47 @@
 import * as bootstrap from 'bootstrap'
 
 // Remove the transition class
-const count = document.querySelector('.status-fire-js');
-count.classList.remove('animation');
+// const count = document.querySelector('.status-fire-js');
+// count.classList.remove('animation');
 
-// Create the observer, same as before:
-const observer = new IntersectionObserver(entries => {
-  entries.forEach(entry => {
+// // Observa o contador, quando é visto inicia a contagem
+// const observer = new IntersectionObserver(entries => {
+//   entries.forEach(entry => {
+//     if (entry.isIntersecting) {
+//       count.classList.add('animation');
+//       return;
+//     }
+
+//     count.classList.remove('animation');
+//   });
+// });
+
+// observer.observe(document.querySelector('.count-wrapper'));
+
+
+
+// Observa o contador, quando é visto inicia a contagem
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    const square = entry.target.querySelector(".status-fire-js");
+
     if (entry.isIntersecting) {
-      count.classList.add('animation');
-      return;
+      square.classList.add("animation");
+      return; // if we added the class, exit the function
     }
 
-    count.classList.remove('animation');
+    // We're not intersecting, so remove the class!
+    square.classList.remove("animation");
   });
 });
 
-observer.observe(document.querySelector('.count-wrapper'));
+let elements = document.querySelectorAll(".count-wrapper");
+for (let element of elements) {
+  observer.observe(element);
+}
 
+
+// Mudanças no carousel bootstrap, controle de cards em vez de slides
 let items = document.querySelectorAll('.carousel .carousel-item')
 
 		items.forEach((el) => {
